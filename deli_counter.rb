8d1a9777c5c76a3da3cf6d0d72a8558fcedd46a6
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-28 22:20:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    number = 1
     katz_deli.each do |customer|
-      
+      line_positions << "#{number}. #{customer}"
     
   end
 def take_a_number(katz_deli,name)
