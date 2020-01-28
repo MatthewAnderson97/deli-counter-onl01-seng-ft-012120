3b1ee5c69b0ae73ad220b93a68c410d56f3ec922
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-28 22:27:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,8 @@ end
   end
 def take_a_number(katz_deli,name)
   katz_deli<< name
-  puts 
+  puts puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
   
   
   def now_serving
